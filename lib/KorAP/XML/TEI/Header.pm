7a79de84528d53fa767bbaa24b08180234f7a80a
Akron <nils@diewald-online.de>
2020-07-27 10:37:52 +0200

    Establish header object for corpus, doc and text header parsing
    Change-Id: I26767fc27054bd3f1b70a622557c0b2f04cac816

---
+++ lib/KorAP/XML/TEI/Header.pm
@@ -0,0 +1,185 @@
+package KorAP::XML::TEI::Header;
+use strict;
+use warnings;
+use Encode qw(encode_utf8);
+
+# Parsing of i5 header files
+
+# Warning:
+# Opening and closing tags (without attributes) have to be in one line
+
+my $_HEADER_TAG = 'idsHeader';
+
+use constant {
+  TEXT     => 0,
+  SIGTYPE  => 1,
+  DIR      => 2,
+  SIGLE    => 3
+};
+
+# convert '&', '<' and '>' into their corresponding sgml-entities
+our %ent = (
+  '"' => '&quot;',
+  '&' => '&amp;',
+  '<' => '&lt;',
+  '>' => '&gt;'
+);
+
+# convert header type to sigle type
+our %sig = (
+  corpus   => 'korpusSigle',
+  document => 'dokumentSigle',
+  text     => 'textSigle'
+);
+
+
+# Create new header object
+sub new {
+  my $class = shift;
+  my $text = shift;
+
+  my $self = bless [$text, undef, '', ''], $class;
+
+  # Check header types to distinguish between siglen types
+  if ($text =~ /^\<${_HEADER_TAG}\stype=\"([^"]+?)\"/) {
+    $self->[SIGTYPE] = $sig{$1} // 'textSigle';
+  }
+
+  # Unexpected header init
+  else {
+    die "ERROR ($0): Unable to parse header init '$text'";
+    return;
+  };
+
+  return $self;
+};
+
+
+# Parse header object from filehandle
+sub parse {
+  my ($self, $fh) = @_;
+
+  my $sig_type = $self->[SIGTYPE];
+
+  # Iterate over file handle
+  while (<$fh>) {
+
+    # Change:
+    #   This version keeps comments in header files
+
+    # End of header found - finish parsing
+    if ( m#^(.*</${_HEADER_TAG}>)(.*)$# ){
+
+      # Add to text
+      $self->[TEXT] .= $1;
+
+      die "ERROR ($0): main(): input line number $.: line with closing header tag '${_HEADER_TAG}'"
+        ." contains additional information ... => Aborting\n\tline=$_"
+        if $2 !~ /^\s*$/;
+
+      if ($self->[DIR] eq '') {
+
+        print STDERR "WARNING ($0): main(): input line number $.: empty " . $sig_type .
+          " in header => nothing to do ...\n header=" . $self->[TEXT] . "\n";
+        return;
+
+      };
+
+      return $self;
+    };
+
+    # Check for sigle in line
+    if ( m#^(.*)<$sig_type(?: [^>]*)?>([^<]*)(.*)$# ){
+
+      my $pfx = $1;
+      my $dir = $2;
+      my $sfx = $3;
+
+      die "ERROR ($0): main(): input line number $.: line with sigle-tag is not in expected format ... => Aborting\n\tline=$_"
+        if $pfx !~ /^\s*$/  || $sfx !~ m#^</$sig_type>\s*$# || $dir =~ /^\s*$/;
+
+      my $sig = encode_utf8($dir);
+      $self->[SIGLE] = $sig;
+
+      # Transliterate directory
+      $dir =~ tr/\./\//;
+
+      $self->[DIR] = $dir;
+
+      # Escape sig
+      $sig =~ s/("|&|<|>)/$ent{$1}/g;
+
+      # Replace line with new dir
+      # Change:
+      #   This now also replaces the sig even if no escaping was taken place,
+      #   so that the change "/" -> _ takes effect
+      s#(<$sig_type(?: [^>]*)?>)[^<]+</$sig_type>#$1$sig</$sig_type>#;
+    };
+
+    # Add line to header text
+    $self->[TEXT] .= $_;
+  };
+};
+
+
+# Directory (leveled) of the header file
+sub dir {
+  $_[0]->[DIR];
+};
+
+
+# corpus/doc/text sigle
+sub sigle {
+  $_[0]->[SIGLE];
+};
+
+
+# corpus/doc/text id
+sub id {
+  $_[0]->[SIGLE] =~ tr/\//_/r;
+};
+
+
+# corpus/doc/text sigle escaped
+sub sigle_esc {
+  $_[0]->[SIGLE] =~ s/("|&|<|>)/$ent{$1}/gr;
+};
+
+
+# corpus/doc/text id escaped
+sub id_esc {
+  $_[0]->[SIGLE] =~ tr/\//_/r =~ s/("|&|<|>)/$ent{$1}/gr;
+};
+
+
+# Return data as a string
+sub to_string {
+  my $self = shift;
+  return $self->_header . $self->[TEXT];
+};
+
+
+# Header for XML output
+sub _header {
+  my $self = shift;
+  ## TODO: remove this, because it's IDS-specific
+  return <<"HEADER";
+<?xml version="1.0" encoding="UTF-8"?>
+<?xml-model href="header.rng"
+            type="application/xml"
+            schematypens="http://relaxng.org/ns/structure/1.0"?>
+<!DOCTYPE idsCorpus PUBLIC "-//IDS//DTD IDS-XCES 1.0//EN"
+          "http://corpora.ids-mannheim.de/idsxces1/DTD/ids.xcesdoc.dtd">
+HEADER
+};
+
+
+# Write data to zip stream
+sub to_zip {
+  my ($self, $zip) = @_;
+  $zip->print(encode_utf8($self->to_string));
+};
+
+
+1;
+
