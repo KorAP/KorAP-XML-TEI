9034d4d55c5eb2d8b659f507465d91f089f72a13
Akron <nils@diewald-online.de>
2020-07-29 11:01:03 +0200

    Improve i5 template testing
    Change-Id: I1bfa22acf3ff9173d26edd9c0edaefe4c7fe3208

--- lib/Test/KorAP/XML/TEI.pm
+++ lib/Test/KorAP/XML/TEI.pm
@@ -4,7 +4,12 @@ use warnings;
 use File::Temp qw/tempfile/;
 use Exporter 'import';
 
-our @EXPORT_OK = qw(korap_tempfile);
+our @EXPORT_OK = qw(korap_tempfile i5_template);
+
+our $data;
+unless ($data) {
+  $data .= <DATA> while !eof(DATA);
+};
 
 # Create a temporary file and file handle
 # That will stay intact, if KORAPXMLTEI_DONTUNLINK is set to true.
@@ -22,4 +27,69 @@ sub korap_tempfile {
   )
 };
 
+
+# Return basic i5 document with replacable parts.
+# Supports:
+# - korpusSigle
+# - dokumentSigle
+# - textSigle
+# - text
+sub i5_template {
+  my %replace = @_;
+  my $tpl = $data;
+
+  foreach my $key (keys %replace) {
+    $tpl =~ s!<% $key %>!$replace{$key}!ge;
+  };
+
+  for ($tpl) {
+    s!<% korpusSigle %>!AAA!g;
+    s!<% dokumentSigle %>!AAA/BBB!g;
+    s!<% textSigle %>!AAA/BBB.00000!g;
+    s!<% text %>!Lorem ipsum!g;
+  };
+
+  return $tpl;
+};
+
+
 1;
+
+
+__DATA__
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE idsCorpus
+  PUBLIC
+  "-//IDS//DTD IDS-I5 1.0//EN"
+  "http://corpora.ids-mannheim.de/I5/DTD/i5.dtd">
+<idsCorpus>
+  <idsHeader type="corpus">
+    <fileDesc>
+      <titleStmt>
+        <korpusSigle><% korpusSigle %></korpusSigle>
+      </titleStmt>
+    </fileDesc>
+  </idsHeader>
+  <idsDoc version="1.0">
+    <idsHeader type="document">
+      <fileDesc>
+        <titleStmt>
+          <dokumentSigle><% dokumentSigle %></dokumentSigle>
+        </titleStmt>
+      </fileDesc>
+    </idsHeader>
+    <idsText version="1.0">
+      <idsHeader type="text">
+        <fileDesc>
+          <titleStmt>
+            <textSigle><% textSigle %></textSigle>
+          </titleStmt>
+        </fileDesc>
+      </idsHeader>
+      <text>
+        <% text %>
+      </text>
+    </idsText>
+  </idsDoc>
+</idsCorpus>
+__END__
