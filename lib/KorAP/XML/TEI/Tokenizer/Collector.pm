76ab94fd4217e7d49e8860e786337f2982d857f7
Akron <nils@diewald-online.de>
2020-08-01 21:05:25 +0200

    Split out structure parsing
    Change-Id: Ia38a8c7f588c5361bebbcc7350c18f1540357757

--- lib/KorAP/XML/TEI/Tokenizer/Collector.pm
+++ lib/KorAP/XML/TEI/Tokenizer/Collector.pm
@@ -5,9 +5,19 @@ use Encode qw(encode decode);
 use strict;
 use warnings;
 
+use constant {
+  WITH_INLINE => 1,
+  STRUCTURE   => 2
+};
 
-# Add token to tokens list
+
 sub add_token {
+  return add_new_annotation(@_);
+};
+
+
+# Add new annotation to annotation list
+sub add_new_annotation {
   my $self = shift;
   my $token = KorAP::XML::TEI::Tokenizer::Token->new(@_);
   push @$self, $token;
@@ -15,15 +25,22 @@ sub add_token {
 };
 
 
+# Add annotation to annotation list
+sub add_annotation {
+  push @{$_[0]}, $_[1];
+};
+
+
 # Get last token added to the tokens list
 sub last_token {
+  # DEPRECATED
   $_[0]->[$#{$_[0]}];
 };
 
 
 # Stringify all tokens
 sub to_string {
-  my ($self, $text_id, $with_inline_annotations) = @_;
+  my ($self, $text_id, $param) = @_;
 
   unless ($text_id) {
     warn 'Missing textID';
@@ -35,27 +52,39 @@ sub to_string {
   # Iterate
   my $c = 0;
 
-
-  # correct last from-value (if the 'second to last'
-  # from-value refers to an s-tag, then the last from-value
-  # is one to big - see retr_info())
-  my $last_token = $self->last_token;
-  if ($last_token->from == $last_token->to + 1) {
-    # TODO:
-    #   check
-    $last_token->set_from($last_token->to);
+  # Correct tokens
+  # TODO:
+  #   Check if this is also necessary for structures
+  if ($param != STRUCTURE) {
+    # correct last from-value (if the 'second to last'
+    # from-value refers to an s-tag, then the last from-value
+    # is one to big - see retr_info())
+    my $last_token = $self->last_token;
+    if ($last_token->from == $last_token->to + 1) {
+      # TODO:
+      #   check
+      $last_token->set_from($last_token->to);
+    };
   };
 
 
-  # Serialize with respect to inline annotations
-  if ($with_inline_annotations) {
+  # Serialize tokens with respect to inline annotations
+  if ($param == WITH_INLINE) {
     # Iterate over all tokens
     foreach (@$self) {
       $output .= $_->to_string_with_inline_annotations($c++);
     };
   }
 
-  # Serialize without respect to inline annotations
+  # Serialize structures
+  elsif ($param == STRUCTURE) {
+    # Iterate over all structures
+    foreach (@$self) {
+      $output .= $_->to_string_as_struct($c++);
+    };
+  }
+
+  # Serialize tokens without respect to inline annotations
   else {
     # Iterate over all tokens
     foreach (@$self) {
@@ -75,8 +104,8 @@ sub boundaries {
 
 # Write data to zip stream (as utf8)
 sub to_zip {
-  my ($self, $zip, $text_id, $with_inline_annotations) = @_;
-  $zip->print(encode('UTF-8', $self->to_string($text_id, $with_inline_annotations)));
+  my ($self, $zip, $text_id, $param) = @_;
+  $zip->print(encode('UTF-8', $self->to_string($text_id, $param)));
 };
 
 
