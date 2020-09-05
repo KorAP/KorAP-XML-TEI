f34d8f68aec5a9a8dd5b163789760b3cbb43dc4e
Marc Kupietz <kupietz@ids-mannheim.de>
2020-09-05 13:51:22 +0200

    Send <EOT>\n to external tokenizer for separating texts
    This is now the standard for the KorAP tokenizer.
    
    Change-Id: I30c2d6ca82211b1d312364899d4f56ea7908b4f8

--- t/cmd/tokenizer.pl
+++ t/cmd/tokenizer.pl
@@ -15,9 +15,11 @@ my $tok = KorAP::XML::TEI::Tokenizer::Aggressive->new;
 # Read lines from input and return boundaries
 while (!eof(STDIN)) {
   my $line = <>;
-  $tok->tokenize($line);
-  print join(' ', $tok->boundaries), "\n";
-  $tok->reset;
+  for my $text (split(/\n?\x{04}\n?/, $line)) {
+    $tok->tokenize($text);
+    print join(' ', $tok->boundaries), "\n";
+    $tok->reset;
+  }
 };
 
 1;
