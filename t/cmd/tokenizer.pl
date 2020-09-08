9693861280d84c66a637401a9f3b1397af1b140c
Marc Kupietz <kupietz@ids-mannheim.de>
2020-09-09 00:05:46 +0200

    Add -tk option to use the standard KoAP tokenizer
    Change-Id: I992fe37463926c8ecbca933fbb709f8640d6fb93

--- t/cmd/tokenizer.pl
+++ t/cmd/tokenizer.pl
@@ -2,6 +2,7 @@
 use strict;
 use warnings;
 use FindBin;
+use Encode;
 BEGIN {
   unshift @INC, "$FindBin::Bin/../../lib";
 };
@@ -14,7 +15,7 @@ my $tok = KorAP::XML::TEI::Tokenizer::Aggressive->new;
 
 # Read lines from input and return boundaries
 while (!eof(STDIN)) {
-  my $line = <>;
+  my $line = decode_utf8(<>);
   for my $text (split(/\n?\x{04}\n?/, $line)) {
     $tok->tokenize($text);
     print join(' ', $tok->boundaries), "\n";
